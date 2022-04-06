4d3bbcb2661356222268fb139af764e842c1c40b
NiceOnion <74778819+NiceOnion@users.noreply.github.com>
2022-04-06 10:19:56 +0200

    .JAR file added. Nu dan eindelijk CI/CD af???

--- core/src/com/mygdx/game/BusinessLayer/Card.java
+++ core/src/com/mygdx/game/BusinessLayer/Card.java
@@ -1,4 +1,29 @@
 package com.mygdx.game.BusinessLayer;
 
 public class Card {
+     private String name;
+     private int health;
+     private int damage;
+     private enum ability{}
+
+     public int getHealth(){
+         return health;
+     }
+
+     public int getDamage(){
+         return damage;
+     }
+
+     public String getName(){
+         return name;
+     }
+
+     public void updateHealth(int resultHealth){
+         health = resultHealth;
+     }
+
+     //public void attack(Card target){
+     //    int resultHealth = target.getHealth() - this.damage;
+     //    target.updateHealth(resultHealth);
+     //}
 }
